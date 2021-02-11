74c96003af49bac89c0d5aea25d80f2a8e5b90ce
Devin Gunay <devingunay@gmail.com>
2021-02-10 21:35:38 -0800

    minor updates

--- gains.py
+++ gains.py
@@ -2,11 +2,23 @@ import cbpro
 import json
 import sys
 
+# How to use:
+#
+# python gains.py <currency> <credentials file>
+#
+#   currency is e.g. BTC-USD for Bitcoin.
+#   credentials file should be JSON in this form:
+#   {
+#       "key": "",
+#       "passphrase": "",
+#       "secret" : ""
+#   }
+
 # Step 0: args
 currency = sys.argv[1]
 cred_file = sys.argv[2]
 
-# Step 1: auth with coinbase
+# Step 1: auth with Coinbase
 with open(cred_file) as f:
     creds = json.load(f)
 
@@ -14,12 +26,11 @@ private_client = cbpro.AuthenticatedClient(creds['key'], creds['secret'], creds[
 
 # Step 2: Get the fills for the chosen currency
 fills_generator = private_client.get_fills(currency)
-fills = list(fills_generator)
 
 # Step 3: Gather the information we need to calculate profit/loss
 usd_spent = 0
 coins_accumulated = 0
-for fill in fills:
+for fill in fills_generator:
     usd_spent += float(fill['usd_volume'])
     coins_accumulated += float(fill['size']) # TODO: check if sells are negative
     pass
@@ -27,11 +38,11 @@ for fill in fills:
 # Step 4: Get the current price of our currency
 price = float(private_client.get_product_ticker(currency)['price'])
 
-# Step 5: Do our calculation
+# Step 5: Do our calculations
 cost_basis = (usd_spent / coins_accumulated)
-profit_loss = ((price / cost_basis) - 1.0) * 100
+profit_loss = ((price / cost_basis) - 1.0) * 100.0
 
-# Print something pretty!
+# Step 6: Print it out nicely!
 print("You spent ${:.2f} USD to buy {} {}".format(usd_spent, coins_accumulated, currency))
 print("Your cost basis is ${:.2f} per {}".format(cost_basis, currency))
 print("The current price is ${:.2f}, leaving you with a return of {:.2f}%".format(price, profit_loss))
